7c0c00a1adea666dc00dc9d570ffd2e464859ac9
Bierque Jason <bierquejason@gmail.com>
2024-01-21 23:40:28 +0100

    Mise à jour des dépendances

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -4,13 +4,13 @@ dependencies {
     implementation(project(":nms:v1_19_R3"))
     implementation(project(":nms:v1_20_R1"))
     implementation(project(":nms:v1_20_R2"))
-    implementation("net.kyori:adventure-text-minimessage:4.14.0")
-    implementation("com.electronwill.night-config:toml:3.6.6")
+    implementation("net.kyori:adventure-text-minimessage:4.15.0")
+    implementation("com.electronwill.night-config:toml:3.6.7")
     implementation("com.google.guava:guava:33.0.0-jre")
-    implementation("org.apache.logging.log4j:log4j-api:2.22.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.22.0")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2")
-    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.22.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.22.1")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.3.2")
+    implementation("com.zaxxer:HikariCP:5.1.0")
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.9") { isTransitive = false }
     compileOnly("com.sk89q.worldedit:worldedit-core:7.2.0-SNAPSHOT") { isTransitive = false }
     compileOnly("net.essentialsx:EssentialsXSpawn:2.19.7") { isTransitive = false }
