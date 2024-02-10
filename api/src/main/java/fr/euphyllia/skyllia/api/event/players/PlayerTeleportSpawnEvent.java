f34c7a27e76ac1f644eee7088468a1f2424f119b
Bierque Jason <bierquejason@gmail.com>
2024-02-11 00:48:34 +0100

    A l'avenir, je n'utiliserai plus EssentialsX, d'où le PlayerTeleportSpawnEvent

---
+++ api/src/main/java/fr/euphyllia/skyllia/api/event/players/PlayerTeleportSpawnEvent.java
@@ -0,0 +1,67 @@
+package fr.euphyllia.skyllia.api.event.players;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Call when the player returns to the spawn using the plugin.
+ */
+public class PlayerTeleportSpawnEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlerList = new HandlerList();
+    private final Player bPlayer;
+    private Location finalLocation;
+    private boolean cancel = false;
+
+    public PlayerTeleportSpawnEvent(Player player, Location location) {
+        this.bPlayer = player;
+        this.finalLocation = location;
+    }
+
+    public Player getPlayer() {
+        return this.bPlayer;
+    }
+
+    public Location getFinalLocation() {
+        return finalLocation;
+    }
+
+    public void setFinalLocation(Location finalLocation) {
+        this.finalLocation = finalLocation;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlerList;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return getHandlerList();
+    }
+
+    /**
+     * Gets the cancellation state of this event. A cancelled event will not
+     * be executed in the server, but will still pass to other plugins
+     *
+     * @return true if this event is cancelled
+     */
+    @Override
+    public boolean isCancelled() {
+        return this.cancel;
+    }
+
+    /**
+     * Sets the cancellation state of this event. A cancelled event will not
+     * be executed in the server, but will still pass to other plugins.
+     *
+     * @param cancel true if you wish to cancel this event
+     */
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
+    }
+}
--- plugin/src/main/java/fr/euphyllia/skyllia/utils/PlayerUtils.java
+++ plugin/src/main/java/fr/euphyllia/skyllia/utils/PlayerUtils.java
@@ -3,28 +3,38 @@ package fr.euphyllia.skyllia.utils;
 import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.spawn.EssentialsSpawn;
 import fr.euphyllia.skyllia.Main;
+import fr.euphyllia.skyllia.api.event.players.PlayerTeleportSpawnEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.World;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.jetbrains.annotations.Nullable;
 
 public class PlayerUtils {
 
     public static void teleportPlayerSpawn(Main main, Player player) {
         player.getScheduler().run(main, scheduledTask -> {
-            World world = Bukkit.getWorlds().get(0);
-            try {
-                EssentialsSpawn essentialsSpawn = (EssentialsSpawn) Bukkit.getPluginManager().getPlugin("EssentialsSpawn");
-                Essentials essentials = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
-                if (essentialsSpawn != null && essentialsSpawn.isEnabled() && essentials != null && essentials.isEnabled()) {
-                    player.teleportAsync(essentialsSpawn.getSpawn(essentials.getUser(player.getUniqueId()).getGroup()), PlayerTeleportEvent.TeleportCause.PLUGIN);
-                } else {
-                    player.teleportAsync(world.getSpawnLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN);
-                }
-            } catch (Exception e) {
-                player.teleportAsync(world.getSpawnLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN);
+            Location spawnLocation = getSpawnLocationEssentials(player); // Todo Sera supprimer à l'avenir, je veux utiliser le point de dépendance possible !
+            if (spawnLocation == null) {
+                spawnLocation = Bukkit.getWorlds().get(0).getSpawnLocation();
             }
+            PlayerTeleportSpawnEvent playerTeleportSpawnEvent = new PlayerTeleportSpawnEvent(player, spawnLocation);
+            Bukkit.getPluginManager().callEvent(playerTeleportSpawnEvent);
+            if (playerTeleportSpawnEvent.isCancelled()) {
+                return;
+            }
+            player.teleportAsync(playerTeleportSpawnEvent.getFinalLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN);
         }, null);
     }
 
+    @Deprecated(forRemoval = true, since = "1.0-RC4")
+    private static @Nullable  Location getSpawnLocationEssentials(Player player) {
+        EssentialsSpawn essentialsSpawn = (EssentialsSpawn) Bukkit.getPluginManager().getPlugin("EssentialsSpawn");
+        Essentials essentials = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
+        if (essentialsSpawn != null && essentialsSpawn.isEnabled() && essentials != null && essentials.isEnabled()) {
+            return essentialsSpawn.getSpawn(essentials.getUser(player.getUniqueId()).getGroup());
+        } else {
+            return null;
+        }
+    }
 }
