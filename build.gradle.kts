e70e9738ddc92c3f01c709a3a2ef7edd63bd79a9
Bierque Jason <bierquejason@gmail.com>
2024-02-11 14:27:08 +0100

    Version 1.0-RC3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "fr.euphyllia";
-version = "1.0-RC4-SNAPSHOT";
+version = "1.0-RC3.2";
 description = "Plugin Skyblock pour Folia / PaperMC";
 
 val paperRepo = "https://repo.papermc.io/repository/maven-public/";
