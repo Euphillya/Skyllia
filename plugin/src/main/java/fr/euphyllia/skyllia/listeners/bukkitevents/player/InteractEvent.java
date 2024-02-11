e6bed3e80c44ab29b4c93ab3cc99063db7811fb1
Bierque Jason <bierquejason@gmail.com>
2024-02-11 12:51:58 +0100

    Oublie d'utiliser la permission pour utiliser la redstone

--- plugin/src/main/java/fr/euphyllia/skyllia/listeners/bukkitevents/player/InteractEvent.java
+++ plugin/src/main/java/fr/euphyllia/skyllia/listeners/bukkitevents/player/InteractEvent.java
@@ -5,11 +5,14 @@ import fr.euphyllia.skyllia.api.skyblock.model.permissions.PermissionsIsland;
 import fr.euphyllia.skyllia.listeners.ListenersUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -31,6 +34,16 @@ public class InteractEvent implements Listener {
         if (player.hasPermission("skyllia.interact.bypass")) {
             return;
         }
+        Block block = event.getClickedBlock();
+        if (block != null) {
+            Material material = block.getType();
+            if ((event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_BLOCK) && (material == Material.COMPARATOR || material == Material.REPEATER)
+                    || event.getAction() == Action.PHYSICAL && (material == Material.TRIPWIRE)) {
+                ListenersUtils.checkPermission(player.getLocation(), player, PermissionsIsland.REDSTONE, event);
+                return;
+            }
+        }
+
         ListenersUtils.checkPermission(player.getLocation(), player, PermissionsIsland.INTERACT, event);
     }
 
