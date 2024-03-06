7ea75282c56efe29edbc8f16819719e18d51a311
Euphyllia Bierque <bierque.euphyllia@gmail.com>
2024-03-06 13:44:45 +0100

    Snapshot 1.0-RC6

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "fr.euphyllia";
-version = "1.0-RC5.2";
+version = "1.0-RC6-SNAPSHOT";
 description = "Plugin Skyblock pour Folia / PaperMC";
 
 val paperRepo = "https://repo.papermc.io/repository/maven-public/";
