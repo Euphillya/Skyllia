01113e4bf2272503313b44ec7a0c8ea5e0d78e59
Euphyllia Bierque <bierque.euphyllia@gmail.com>
2024-04-09 16:19:16 +0200

    Pour le moment, on reste sur Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,6 +79,6 @@ tasks.test {
 
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
