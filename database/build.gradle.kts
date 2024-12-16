e4a0e6649a9b56b334723eed037a28d86cd8acb7
Euphyllia Bierque <bierque.euphyllia@gmail.com>
2024-12-16 01:04:08 +0100

    Database dont need Paper API

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -4,10 +4,6 @@ plugins {
 group = "fr.euphyllia.skyllia";
 version = "1.3";
 
-dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.3-R0.1-SNAPSHOT")
-}
-
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
