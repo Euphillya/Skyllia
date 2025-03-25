04fec114a79feed333035c56d73a3e29c4cd5b86
Euphyllia Bierque <bierque.euphyllia@gmail.com>
2025-03-25 01:21:15 +0100

    Add FAWE in paper plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -15,8 +15,8 @@ dependencies {
     compileOnly(project(":nms:v1_21_R3"))
     compileOnly(project(":nms:v1_21_R4"))
 
-    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.3.10") { isTransitive = false }
-    compileOnly("com.sk89q.worldedit:worldedit-core:7.3.9") { isTransitive = false }
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.3.12-SNAPSHOT") { isTransitive = false }
+    compileOnly("com.sk89q.worldedit:worldedit-core:7.3.12-SNAPSHOT") { isTransitive = false }
 }
 
 java {
