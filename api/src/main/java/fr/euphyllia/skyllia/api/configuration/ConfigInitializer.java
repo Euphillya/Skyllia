cc41b8ebd415fec1bc48c840298d5bf07d2c000f
Euphyllia Bierque <bierque.euphyllia@gmail.com>
2025-05-11 18:27:21 +0200

    Remove old code

--- api/src/main/java/fr/euphyllia/skyllia/api/configuration/ConfigInitializer.java
+++
@@ -1,8 +0,0 @@
-package fr.euphyllia.skyllia.api.configuration;
-
-import java.io.File;
-
-@FunctionalInterface
-public interface ConfigInitializer {
-    void initConfig(File configFile) throws Exception;
-}
