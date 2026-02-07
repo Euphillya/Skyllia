74b4ca54a471356bd0c58a5ed83816c2161fb3ae
Bierque Euphyllia <bierque.euphyllia@gmail.com>
2026-02-07 23:56:14 +0100

    Nouvel addon : Ajout d'un coffre d'ile

---
+++ addons/SkylliaChest/src/main/java/fr/euphyllia/skylliachest/SkylliaChest.java
@@ -0,0 +1,11 @@
+package fr.euphyllia.skylliachest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkylliaChest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
