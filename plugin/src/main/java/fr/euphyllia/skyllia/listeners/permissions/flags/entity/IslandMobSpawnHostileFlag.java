9aa816af56cdd96a8b31a764c255acf3fde79acd
Bierque Euphyllia <bierque.euphyllia@gmail.com>
2026-03-09 20:40:27 +0100

    Class HOSTILE

---
+++ plugin/src/main/java/fr/euphyllia/skyllia/listeners/permissions/flags/entity/IslandMobSpawnHostileFlag.java
@@ -0,0 +1,47 @@
+package fr.euphyllia.skyllia.listeners.permissions.flags.entity;
+
+import fr.euphyllia.skyllia.api.permissions.FlagId;
+import fr.euphyllia.skyllia.api.permissions.IslandFlagRegistry;
+import fr.euphyllia.skyllia.api.permissions.modules.FlagModule;
+import org.bukkit.plugin.Plugin;
+
+public class IslandMobSpawnHostileFlag implements FlagModule {
+
+    private FlagId ALLOW_SPAWN_ALL_HOSTILE;
+    private FlagId ALLOW_SPAWN_BLAZE;
+    private FlagId ALLOW_SPAWN_BOGGED;
+    private FlagId ALLOW_SPAWN_BREEZE;
+    private FlagId ALLOW_SPAWN_CREAKING;
+    private FlagId ALLOW_SPAWN_CREEPER;
+    private FlagId ALLOW_SPAWN_ELDER_GUARDIAN;
+    private FlagId ALLOW_SPAWN_ENDERMITE;
+    private FlagId ALLOW_SPAWN_EVOKER;
+    private FlagId ALLOW_SPAWN_GHAST;
+    private FlagId ALLOW_SPAWN_GUARDIAN;
+    private FlagId ALLOW_SPAWN_HOGLIN;
+    private FlagId ALLOW_SPAWN_HUSK;
+    private FlagId ALLOW_SPAWN_MAGMA_CUBE;
+    private FlagId ALLOW_SPAWN_PARCHED;
+    private FlagId ALLOW_SPAWN_PHANTOM;
+    private FlagId ALLOW_SPAWN_PIGLIN_BRUTE;
+    private FlagId ALLOW_SPAWN_PILLAGER;
+    private FlagId ALLOW_SPAWN_RAVAGER;
+    private FlagId ALLOW_SPAWN_SHULKER;
+    private FlagId ALLOW_SPAWN_SILVERFISH;
+    private FlagId ALLOW_SPAWN_SKELETON;
+    private FlagId ALLOW_SPAWN_SLIME;
+    private FlagId ALLOW_SPAWN_STRAY;
+    private FlagId ALLOW_SPAWN_VEX;
+    private FlagId ALLOW_SPAWN_VINDICATOR;
+    private FlagId ALLOW_SPAWN_WARDEN;
+    private FlagId ALLOW_SPAWN_WITCH;
+    private FlagId ALLOW_SPAWN_WITHER_SKELETON;
+    private FlagId ALLOW_SPAWN_ZOGLIN;
+    private FlagId ALLOW_SPAWN_ZOMBIE;
+    private FlagId ALLOW_SPAWN_ZOMBIE_VILLAGER;
+
+    @Override
+    public void registerFlags(IslandFlagRegistry registry, Plugin owner) {
+
+    }
+}
