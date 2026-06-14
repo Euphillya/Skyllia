65cfe5a389bb715bee45cee3da6472a9d9d4b0f3
Magic_Bili <124496967+addpromax@users.noreply.github.com>
2026-06-14 11:45:47 +0800

    fix class error

--- addons/SkylliaOre/src/main/java/fr/euphyllia/skylliaore/hook/CraftEngineHook.java
+++ addons/SkylliaOre/src/main/java/fr/euphyllia/skylliaore/hook/CraftEngineHook.java
@@ -1,9 +1,8 @@
 package fr.euphyllia.skylliaore.hook;
 
-import net.momirealms.craftengine.bukkit.block.BukkitBlockManager;
 import net.momirealms.craftengine.bukkit.util.BlockStateUtils;
 import net.momirealms.craftengine.core.block.BlockDefinition;
-import net.momirealms.craftengine.core.block.ImmutableBlockState;
+import net.momirealms.craftengine.core.block.BlockManager;
 import net.momirealms.craftengine.core.util.Key;
 import org.bukkit.block.data.BlockData;
 import org.jetbrains.annotations.Nullable;
@@ -22,19 +21,17 @@ public class CraftEngineHook {
     @Nullable
     public static BlockData getBlockData(String id) {
         try {
-            BukkitBlockManager blockManager = BukkitBlockManager.instance();
+            BlockManager blockManager = BlockManager.instance();
             if (blockManager == null) {
                 return null;
             }
 
-            // Parse block ID and properties
+            // Parse block ID (ignore properties for now, just get the base block)
             String blockId;
-            String properties = null;
             
             int bracketIndex = id.indexOf('[');
             if (bracketIndex > 0) {
                 blockId = id.substring(0, bracketIndex);
-                properties = id.substring(bracketIndex);
             } else {
                 blockId = id;
             }
@@ -49,26 +46,12 @@ public class CraftEngineHook {
 
             BlockDefinition blockDef = blockDefOpt.get();
             
-            // Get default state
-            ImmutableBlockState blockState = blockDef.variantProvider().getDefaultState();
+            // Get default state and convert to BlockData
+            // The visual block state is what the client sees
+            Object minecraftState = blockDef.defaultState().visualBlockState().minecraftState();
             
-            // If properties are specified, try to parse them
-            if (properties != null && !properties.isEmpty()) {
-                // Try to create using the full state string
-                var fullState = blockManager.createBlockState(id);
-                if (fullState != null) {
-                    Object minecraftState = fullState.minecraftState();
-                    return BlockStateUtils.fromBlockData(minecraftState);
-                }
-            }
-            
-            // Return BlockData of the default state
-            if (blockState != null && blockState.visualBlockState() != null) {
-                Object minecraftState = blockState.visualBlockState().minecraftState();
-                return BlockStateUtils.fromBlockData(minecraftState);
-            }
-            
-            return null;
+            // Use BlockStateUtils to convert to BlockData
+            return BlockStateUtils.fromBlockData(minecraftState);
         } catch (Exception e) {
             return null;
         }
@@ -81,7 +64,7 @@ public class CraftEngineHook {
      */
     public static boolean isAvailable() {
         try {
-            BukkitBlockManager blockManager = BukkitBlockManager.instance();
+            BlockManager blockManager = BlockManager.instance();
             return blockManager != null;
         } catch (Throwable e) {
             return false;
